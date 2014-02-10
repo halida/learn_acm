2c8bd40e1d5fa8d71fc6ea02e29b23f9807c943b
halida <linjunhalida@gmail.com>
2014-02-10 22:59:21 +0800

    cleanup

--- sep/main.cpp
+++ sep/main.cpp
@@ -1,12 +1,6 @@
 //-*-coding:utf-8-*-
 //----------------------------------------------------------
 // module: main
-// for n > 2, has N = {1,2,..,n}, sep N to two group, 
-// sum of group should equal. find m = count of posible combination of groups. 
-// 
-// input: n, output: count
-// 
-// f(n, t) = f(n-1, t-n) U f(n-1, t+n)
 //----------------------------------------------------------
 #include <iostream>
 using namespace std;
--- template/main.cpp
+++ template/main.cpp
@@ -1,12 +1,6 @@
 //-*-coding:utf-8-*-
 //----------------------------------------------------------
 // module: main
-// for n > 2, has N = {1,2,..,n}, sep N to two group, 
-// sum of group should equal. find m = count of posible combination of groups. 
-// 
-// input: n, output: count
-// 
-// f(n, t) = f(n-1, t-n) U f(n-1, t+n)
 //----------------------------------------------------------
 #include <algorithm>
 #include <vector>
@@ -18,9 +12,6 @@
 
 using namespace std;
 
-const int p = 8;
-const int q = 1;
-const int a[] = {3};
 
 void solve(){
 
